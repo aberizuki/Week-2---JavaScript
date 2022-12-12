dde269ce6be20eaca7e9923a3d2aa99c40a06f5a
aberizuki <72960760+aberizuki@users.noreply.github.com>
2022-12-13 01:16:37 +0700

    Add files via upload

--- Task3.js
+++ Task3.js
@@ -1,21 +1,23 @@
-let mtk = 100;
-let ind = 100;
-let ing = 100;
-let ipa = 100;
+let mtk = 79;
+let ind = 79;
+let ing = 79;
+let ipa = 79;
 tot = (mtk + ind + ing + ipa) / 4;
+
+avg = "Rata rata : " + tot;
 
 if (mtk != "" && ind != "" && ing != "" && ipa != "") {
   if (tot >= 90) {
-    console.log("A");
+    console.log("Grade = A" + "\n" + avg);
   } else if (tot >= 80) {
-    console.log("B");
+    console.log("Grade = B" + "\n" + avg);
   } else if (tot >= 70) {
-    console.log("C");
+    console.log("Grade = C" + "\n" + avg);
   } else if (tot >= 60) {
-    console.log("D");
+    console.log("Grade = D" + "\n" + avg);
   } else if (tot < 60) {
-    console.log("E");
+    console.log("Grade = E" + "\n" + avg);
   }
 } else {
-  console.log("Nilai ada yang kosong");
+  console.log("Semua nilai harus terisi !");
 }
